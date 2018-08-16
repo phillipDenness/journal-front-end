33e649610f518d7b9408dcf0bc452a069c012a01
Phillip Denness <dennessphillip@gmail.com>
2018-08-16 17:29:23 +0100

    Restructure app to match convention

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
-const service = require('./service')
+const routes = require('./routes')
 const app = express()
 
-app.get('/', function (req, res) {    
-    var json = service.getResources(function(json) {
-        res.send(json);
-    });
-})
-
-app.get('/resources', function (req, res) {
-    var json = service.getResources(function (json) {
-        res.send(json);
-    });
-})
+exports.init = function() {
+    app.get('/', function (req, res) {    
+        var json = service.getResources(function(json) {
+            res.send(json);
+        });
+    })
+    
+    app.get('/resources', function (req, res) {
+        var json = service.getResources(function (json) {
+            res.send(json);
+        });
+    })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+};
