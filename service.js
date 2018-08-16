3fbbe1d9269e517f29ba1d601bb0c2a9bc793027
Phillip Denness <dennessphillip@gmail.com>
2018-08-16 14:44:08 +0100

    Initialise nodeJS
    Service, express & rest classes

---
+++ service.js
@@ -0,0 +1,22 @@
+const rest = require('./rest')
+
+exports.getResources = function(callback) {
+    var options = {
+        protocol: 'http',
+        host: 'journaljack-env-1.un4wzhvicb.eu-west-2.elasticbeanstalk.com',
+        port: 80,
+        //host: 'localhost',
+        //port: '8080',
+        path: '/resources',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return rest.getJSON(options, function(err, body) {
+        if(!err) {
+            callback(body);
+        }
+    });
+}
