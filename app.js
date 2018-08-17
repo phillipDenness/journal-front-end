a0216d31f5a9ba8939d21b10056272650c40c0f8
Phillip Denness <dennessphillip@gmail.com>
2018-08-17 13:19:40 +0100

    Resource create form

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const resources = require('./routes/resources.js');
+const resourceRoutes = require('./routes/resourceRoutes');
 const config = require('./config');
-
 const app = express();
+
 app.set('port', config.app.port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('view options', {layout: false});
-app.set('views', __dirname + '/views');
 
 exports.init = function() {
-    app.use('/resources', resources);
+    // Routes
+    app.use('/', resourceRoutes);
 
-app.listen(config.app.port, () => console.log('App listening on port 3000!'))
+app.listen(config.app.port, () => console.log('App listening on port ' + config.app.port + '!'))
 };
